1da089d156847ccf48142e6e85d2862ee4c2e22c
Jegan Matthews <matthewsjegan@gmail.com>
2017-08-02 14:09:51 -0700

    FireBase Config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  // paymentServiceUrl: 'https://payment-server.herokuapp.com',
-  paymentServiceUrl: 'http://localhost:3000',
+  paymentServiceUrl: 'https://payment-server.herokuapp.com',
+  // paymentServiceUrl: 'http://localhost:3000',
   paymentClientUrl: 'http://localhost:4200'
 };;
